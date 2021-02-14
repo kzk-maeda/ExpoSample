9d771c2da7bedd672698362762770aef566af880
kzk-maeda <kzk.maeda0711@gmail.com>
2021-02-14 20:33:28 +0900

    intactive animation

--- src/components/MemoList.jsx
+++ src/components/MemoList.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import {
+  View, Text, StyleSheet, TouchableOpacity,
+} from 'react-native';
 import { Feather } from '@expo/vector-icons';
 
 export default function MemoList() {
@@ -10,9 +12,9 @@ export default function MemoList() {
           <Text style={styles.memoListItemTitle}>ItemList</Text>
           <Text style={styles.memoListItemDate}>2020/12/24 10:00</Text>
         </View>
-        <View>
+        <TouchableOpacity>
           <Feather name="x" size={16} color="#B0B0B0" />
-        </View>
+        </TouchableOpacity>
       </View>
     </View>
   );
